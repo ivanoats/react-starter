b9f2eeb7e1ce4ea7258d5c2ff35d772c8527917d
Ivan Storck <ivanoats@uw.edu>
2015-01-28 10:28:00 -0800

    Hello World Component being loaded via jspm / jsx transform TG-22 #in-progress

---
+++ app/js/components/HelloComponent.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+export default React.createClass({
+  render: function() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+})
--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-console.log('dynamically loaded');
-
-import {hello} from './lib/helloWorld';
-
-hello();
